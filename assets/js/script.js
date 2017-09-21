5376b8d97a9cc4d740bf6a872643ad8e23937f5a
MKA1500 <maciejkabsch@gmail.com>
2017-09-21 17:11:32 +0200

    new changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,10 +5,30 @@ var app =  angular.module("computer", ['ngRoute'])
                 templateUrl: 'main.html',
                 controller: 'MainCtrl'
             })
+            .when('/about', {
+                templateUrl: 'about.html',
+                controller: 'MainCtrl'
+            })
+            .when('/services', {
+                templateUrl: 'services.html',
+                controller: 'ServicesCtrl'
+            })
+            .when('/contact', {
+                templateUrl: 'contact.html',
+                controller: 'ContactCtrl'
+            })
             .otherwise({redirectTo:'/main'});
 
     }])
     .controller('MainCtrl', ['$scope', function($scope){
         console.log('This is the main controller');
-
+    }])
+    .controller('ServicesCtrl', ['$scope', '$http', function($scope, $http){
+        $http.get('services.json').then(function(response){
+            $scope.services = response.data;
+            console.log($scope.services);
+        });
+    }])
+    .controller('ContactCtrl', ['$scope', function($scope){
+        console.log('This is the contact controller');
     }]);
