5ba4a9a9f912f14c639366f88b2a79f6b579fae1
MKA1500 <maciejkabsch@gmail.com>
2018-07-15 15:42:20 +0200

    ng style

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -7,7 +7,16 @@ import { Component } from '@angular/core';
 export class ServerComponent {
   serverId: number = 10;
   serverStatus: string = 'offline';
+
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
+
   gestServerStatus(){
     return this.serverStatus;
+  }
+
+  getColor() {
+    console.log('hi');
   }
 }
