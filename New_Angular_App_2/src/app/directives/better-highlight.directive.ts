6d24d7cc2521711f8f07762f1eae1baffb51e582
MKA1500 <maciejkabsch@gmail.com>
2018-10-03 21:31:05 +0200

    @HostBinding

--- New_Angular_App_2/src/app/directives/better-highlight.directive.ts
+++ New_Angular_App_2/src/app/directives/better-highlight.directive.ts
@@ -1,9 +1,16 @@
-import { Directive, OnInit, ElementRef, Renderer2, HostListener } from '@angular/core';
+import {
+  Directive,
+  OnInit,
+  ElementRef,
+  Renderer2,
+  HostListener,
+  HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
+  @HostBinding('style.backgroundColor') backgroundColor: string = 'transparent';
 
   constructor(private elRef: ElementRef, private renderer: Renderer2) { }
 
@@ -12,10 +19,12 @@ export class BetterHighlightDirective implements OnInit {
   }
 
   @HostListener('mouseenter') mouseover(eventData: Event) {
-    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', '#7fbfe2');
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', '#7fbfe2');
+    this.backgroundColor = '#7fbfe2';
   }
 
   @HostListener('mouseleave') mouseleave(eventData: Event) {
-    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', '#cea9bd');
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', '#cea9bd');
+    this.backgroundColor = '#cea9bd';
   }
 }
