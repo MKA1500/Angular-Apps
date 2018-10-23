ce9aa4f5ef7216c1fa5cd61eff4dbb465556ab0a
MKA1500 <maciejkabsch@gmail.com>
2018-10-23 21:42:35 +0200

    service and user data interface

--- New_Angular_App_2/src/app/services/user.service.ts
+++ New_Angular_App_2/src/app/services/user.service.ts
@@ -1,10 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { User } from '../models/user.model';
 
 
 @Injectable()
 export class UserService {
-  constructor() { }
 
-  //'http://jsonplaceholder.typicode.com/users'
+  private serviceUrl = 'http://jsonplaceholder.typicode.com/users';
+
+  constructor(private http: HttpClient) { }
+
+  getUser(): Observable<User[]> {
+    return this.http.get<User[]>(this.serviceUrl);
+  }
+
 }
