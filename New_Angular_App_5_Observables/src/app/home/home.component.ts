5535f04cba13fc5db5bcb0a6a1b01a4bd278c5eb
MKA1500 <maciejkabsch@gmail.com>
2018-11-24 08:01:10 +0100

    observables excercises

--- New_Angular_App_5_Observables/src/app/home/home.component.ts
+++ New_Angular_App_5_Observables/src/app/home/home.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Observable } from 'rxjs/Observable';
+import { interval } from 'rxjs';
+import 'rxjs/Rx';
+import { Observer } from 'rxjs/Observer';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -10,6 +15,14 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-  }
+    // const source = interval(1000);
+    // //output: 0,1,2,3,4,5....
+    // const subscribe = source.subscribe(val => console.log(val));
 
+    const myObservable = Observable.create((observer: Observer) => {
+      setTimeout(() => {
+        observer.next();
+      }, 2000)
+    });
+  }
 }
