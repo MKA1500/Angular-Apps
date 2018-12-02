f57eea95081ba8fbbe36c1fae7186bcf3b6a62d9
MKA1500 <maciejkabsch@gmail.com>
2018-12-02 10:36:36 +0100

    unsubscribe rxjs

--- New_Angular_App_5_Observables/src/app/home/home.component.ts
+++ New_Angular_App_5_Observables/src/app/home/home.component.ts
@@ -3,19 +3,24 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/Rx';
 import { Observer } from 'rxjs/Observer';
-
+import { Subscription } from 'rxjs/Subscription';
+import { interval } from 'rxjs';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  numbersObsSubscription: Subscription;
+  customObsSubscription: Subscription;
 
   constructor() { }
 
   ngOnInit() {
+    // ERROR TypeError: rxjs_Observable__WEBPACK_IMPORTED_MODULE_1__.Observable.interval is not a function
+
     // const myNumbers = Observable.interval(1000);
-    // myNumbers.subscribe(
+    // this.numbersObsSubscription = myNumbers.subscribe(
     //   (number: number) => {
     //     console.log(number);
     //   }
@@ -35,10 +40,15 @@ export class HomeComponent implements OnInit {
         observer.next('third package');
       }, 4000);
     });
-    myObservable.subscribe(
+    this.customObsSubscription = myObservable.subscribe(
       (data: string) => { console.log(data); },
       (error: string) => { console.log(error); },
       () => { console.log('completed') }
     );
+  }
+
+  ngOnDestroy() {
+    // this.numbersObsSubscription.unsubscribe();
+    this.customObsSubscription.unsubscribe();
   }
 }
