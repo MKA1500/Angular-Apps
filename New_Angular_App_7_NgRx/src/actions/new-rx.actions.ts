9349646f459aec157370930714189a90f57f897a
MKA1500 <maciejkabsch@gmail.com>
2019-01-31 08:19:51 +0100

    bootstrap, model and actions

---
+++ New_Angular_App_7_NgRx/src/actions/new-rx.actions.ts
@@ -0,0 +1,6 @@
+import { Injectable } from '@angular/core';
+import { Action } from '@ngrx/store';
+import {  NewRx } from './../models/new-rx.model';
+
+export const ADD_NEWRX = '[NEWRX] Add';
+export const REMOVE_NEWRX = '[NEWRX] Remove';
---
+++ New_Angular_App_7_NgRx/src/models/new-rx.model.ts
@@ -0,0 +1,4 @@
+export interface NewRx {
+  name: string;
+  url: string;
+}
