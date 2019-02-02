e48e920502abba2ea182b7597187f19d50bd6c08
MKA1500 <maciejkabsch@gmail.com>
2019-02-02 14:04:02 +0100

    more on ngrx

--- New_Angular_App_7_NgRx/src/actions/new-rx.actions.ts
+++ New_Angular_App_7_NgRx/src/actions/new-rx.actions.ts
@@ -1,6 +1,12 @@
 import { Injectable } from '@angular/core';
 import { Action } from '@ngrx/store';
-import {  NewRx } from './../models/new-rx.model';
+import { NewRx } from './../models/new-rx.model';
 
 export const ADD_NEWRX = '[NEWRX] Add';
 export const REMOVE_NEWRX = '[NEWRX] Remove';
+
+export class AddNewrx implements Action {
+  readonly type = ADD_NEWRX;
+
+  constructor(public payload: NewRx) {}
+}
