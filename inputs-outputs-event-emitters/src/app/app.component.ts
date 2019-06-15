c9234508671bbd4f50cab09e8e6fe19c3516daa1
MKA1500 <maciejkabsch@gmail.com>
2019-06-15 10:46:19 +0200

    starting and stoping interval

--- inputs-outputs-event-emitters/src/app/app.component.ts
+++ inputs-outputs-event-emitters/src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'inputs-outputs-event-emitters';
+
+  onIntervalFired(firedNumber: number) {
+    console.log(firedNumber);
+  }
 }
